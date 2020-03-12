bf61569c0ebc86adcc8c1d573ca460645fda5464
ouyang <astlife@hotmail.com>
2020-03-12 19:38:42 +0800

    app test 1

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 const http = require("http");
 
 http.createServer(function (request, response) {
-    console.log(request.url);
+    let url = new URL(request.url);
+    let path = url.pathname;
+    let params = url.searchParams;
+      
+    console.log(path, JSON.stringify(params));
+
+    switch(path) {
+        
+    }
+
 
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('');
