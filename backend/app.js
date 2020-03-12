cdafc033f14c748a58aea26403dc9b7db7061dbe
ouyang <astlife@hotmail.com>
2020-03-12 20:03:43 +0800

    app test 4

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const http = require("http");
+const querystring = require('querystring');
 
 http.createServer(function (request, response) {
     let url = new URL(request.url, `http://${request.headers.host}`);
     let path = url.pathname;
-    let params = url.searchParams;
+    let search = url.search;
+    let params = querystring.decode(search.slice(1));
       
     console.log(path, JSON.stringify(params));
 
